36427adbc80bebe793279bf8fb336d5ce74d89b8
Joakim <joakimedvardsen2000@gmail.com>
2022-01-20 15:31:50 +0100

    Finished exercise 2.1

--- example-01-HelloSpring/src/main/java/no/ntnu/hello/GreetingController.java
+++ example-01-HelloSpring/src/main/java/no/ntnu/hello/GreetingController.java
@@ -1,5 +1,8 @@
 package no.ntnu.hello;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,16 @@ public class GreetingController {
     @GetMapping("/hello")
     public String greeting() {
         return "Hei, World";
+    }
+
+    @GetMapping("/hei")
+    public ResponseEntity<String> hei() {
+        return new ResponseEntity<>(HttpStatus.MOVED_PERMANENTLY);
+    }
+
+    @DeleteMapping("/hello")
+    public ResponseEntity<String> delete() {
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 }
 
