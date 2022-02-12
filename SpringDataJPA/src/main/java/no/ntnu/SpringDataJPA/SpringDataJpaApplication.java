d9761d8bf717348c78e60a6e1d7bd70617b0f853
Joakim <joakimedvardsen2000@gmail.com>
2022-02-12 14:59:36 +0100

    Tested adding and finding employees

--- SpringDataJPA/src/main/java/no/ntnu/SpringDataJPA/SpringDataJpaApplication.java
+++ SpringDataJPA/src/main/java/no/ntnu/SpringDataJPA/SpringDataJpaApplication.java
@@ -1,13 +1,30 @@
 package no.ntnu.SpringDataJPA;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+	private void insertFourEmployees(EmployeeRepository repository) {
+		repository.save(new Employee("Joakim", "Edvardsen"));
+		repository.save(new Employee("Torstein", "Eide"));
+		repository.save(new Employee("Richileu", "Bailey"));
+		repository.save(new Employee("Eduard", "Cristea"));
+	}
+
+	@Bean
+	public CommandLineRunner run(EmployeeRepository repository) {
+		return (args) -> {
+			insertFourEmployees(repository);
+			System.out.println(repository.findAll());
+		};
 	}
 
 }
