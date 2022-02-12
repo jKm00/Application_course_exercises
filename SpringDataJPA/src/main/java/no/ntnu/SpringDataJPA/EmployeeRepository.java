a8d35870e997a516ef27209837b339e4b0735f9d
Joakim <joakimedvardsen2000@gmail.com>
2022-02-12 14:48:14 +0100

    Added configuration to the database

--- SpringDataJPA/src/main/java/no/ntnu/SpringDataJPA/EmployeeRepository.java
+++ SpringDataJPA/src/main/java/no/ntnu/SpringDataJPA/EmployeeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-    List<Employee> findEmployeeByLastNameContaining(String str)
+    List<Employee> findEmployeeByLastNameContaining(String str);
 }
