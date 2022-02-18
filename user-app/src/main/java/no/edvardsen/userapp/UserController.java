6b1581c23d8fff569082e3305a6532a69c169112
Joakim <joakimedvardsen2000@gmail.com>
2022-02-19 00:53:34 +0100

    Added all necessary method for all user classes

--- user-app/src/main/java/no/edvardsen/userapp/UserController.java
+++ user-app/src/main/java/no/edvardsen/userapp/UserController.java
@@ -1,10 +1,53 @@
 package no.edvardsen.userapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Get all users
+     * HTTP GET to /users
+     * @return list of all users currently stored in the database
+     */
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    /**
+     * Add a user to the register
+     * @param user the user to be added, from HTTP response body
+     * @return {@code 200 OK} on success, {@code 400 BAD REQUEST} on error
+     */
+    @PostMapping
+    public ResponseEntity<String> add(@RequestBody User user) {
+        ResponseEntity<String> response;
+        if (userService.add(user)) {
+            response = new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> remove(@PathVariable int id) {
+        ResponseEntity<String> response;
+        if (userService.remove(id)) {
+            response = new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return response;
+    }
 }
