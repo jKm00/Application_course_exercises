ba8c681d00a84544d45b1814178dddebb8a18ec6
Joakim <joakimedvardsen2000@gmail.com>
2022-02-25 14:23:27 +0100

    Created new spring boot project for exercise 7.1

---
+++ ex7.1-application-properties-test/src/main/java/no/ntnu/applicationpropertiestest/ApplicationPropertiesTestApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.applicationpropertiestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPropertiesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationPropertiesTestApplication.class, args);
+	}
+
+}
---
+++ ex7.1-application-properties-test/src/test/java/no/ntnu/applicationpropertiestest/ApplicationPropertiesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.applicationpropertiestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationPropertiesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
