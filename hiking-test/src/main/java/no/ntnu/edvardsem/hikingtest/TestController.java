3e39772780dc3f0d1b5eed58543410d2bb1ce8fb
Joakim <joakimedvardsen2000@gmail.com>
2022-03-03 15:58:55 +0100

    Added greeting endpoint

--- hiking-test/src/main/java/no/ntnu/edvardsem/hikingtest/TestController.java
+++ hiking-test/src/main/java/no/ntnu/edvardsem/hikingtest/TestController.java
@@ -1,4 +1,20 @@
 package no.ntnu.edvardsem.hikingtest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @Value("${application.version}")
+    private String appVersion;
+
+    /**
+     * Returns the current version of the application as a string
+     * @return the current version of the application as a string
+     */
+    @GetMapping
+    public String greeting() {
+        return "Version: " + appVersion;
+    }
 }
