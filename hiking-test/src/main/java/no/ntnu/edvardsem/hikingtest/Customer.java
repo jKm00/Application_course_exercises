d704aec67c5cd3237fdc77c005decab50f08a78e
Joakim <joakimedvardsen2000@gmail.com>
2022-03-03 16:25:52 +0100

    Created customer entity

--- hiking-test/src/main/java/no/ntnu/edvardsem/hikingtest/Customer.java
+++ hiking-test/src/main/java/no/ntnu/edvardsem/hikingtest/Customer.java
@@ -1,7 +1,6 @@
 package no.ntnu.edvardsem.hikingtest;
 
-import org.springframework.data.annotation.Id;
-
+import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 
@@ -12,4 +11,35 @@ public class Customer {
     private int customerId;
     private String firstname;
     private String lastname;
+
+    public Customer() {}
+
+    public Customer(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
